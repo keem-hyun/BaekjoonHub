08bba83edeb2334d691350841c09bd1575578532
keem-hyun <107028610+keem-hyun@users.noreply.github.com>
2023-05-07 17:26:15 +0900

    [Silver V] Title: 영화감독 숌, Time: 32 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.swift
@@ -0,0 +1,21 @@
+let N = Int(readLine()!)!
+
+var count = 0
+for i in 666...10000000 {
+    var n = i
+    while n >= 666 {
+        if n % 1000 == 666 {
+            count += 1
+            break
+        }
+        n /= 10
+    }
+    
+    if count == N {
+        print(i)
+        break
+    }
+    
+    
+}
+
