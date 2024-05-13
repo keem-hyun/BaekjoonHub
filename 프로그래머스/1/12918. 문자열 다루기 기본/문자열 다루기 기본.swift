c853c6950484d6308b96e2360736fd2a7d28d2db
keem-hyun <107028610+keem-hyun@users.noreply.github.com>
2024-05-13 18:03:08 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,8 +1,6 @@
 func solution(_ s:String) -> Bool {
     
-    var result = s.map {$0}
-    
-    if result.count == 4 || result.count == 6 {
+    if s.count == 4 || s.count == 6 {
         return s.allSatisfy{ $0.isNumber }
     } else {
         return false
